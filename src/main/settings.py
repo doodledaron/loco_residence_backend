f36b354c4e89258aada71905aa1e1f178728c79d
daron <daron@alphv.com>
2024-11-03 22:54:12 +0800

    updated settings

--- src/main/settings.py
+++ src/main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'loco-residence-backend.onrender.com']
 
 
 # Application definition
